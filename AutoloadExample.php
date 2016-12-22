321fb123c83381d08c673cb5f6f501896b58e41b
Vadim Mazur <mazurva@gmail.com>
2016-12-22 17:36:19 +0600

    First commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace sibds\payment\alfabank;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
